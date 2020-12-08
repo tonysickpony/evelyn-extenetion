23f3fd39ee7bacff9e9e873a38579a8327efb514
EvelynChapman <69546339+EvelynChapman@users.noreply.github.com>
2020-12-08 11:19:35 +1100

    Added chalange 2
    added new command that works the same as the old one but adds a code to the error, another function cans for these and adds a code action that lets you chose a type signiture and error messaeg are updated to match

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as testJson from './no-instance.json'
+import * as noInstanceJson from './no-instance.json';
+
+import * as incompatiblilityJson from './incompatiblility.json';
+import * as IncompatibilityNumberJson from './Incompatibility-chooseNumber.json';
+import * as IncompatibilityStringJson from './Incompatibility-chooseString.json';
+
+const CHECKINGTYPE = 'checkingType';
+const DOCUMENT = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.document : false;
+
+interface error {
+	message: string
+	locations: location[]
+}
+interface location{
+	fromLine: number
+	toLine: number
+	fromColumn: number
+	toColumn: number
+}
+
+interface parsedJson {
+	fileName: string
+	moduleName: string
+	errors: error[] 
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,73 +33,128 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('to terminal');
 
-	console.log(testJson)
-
-	interface Error {
-		message: string
-		
-	}
+	const diagnosticsSet = vscode.languages.createDiagnosticCollection("haskellErrors");
+	context.subscriptions.push(diagnosticsSet);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('haskell-debugging.showHighlight', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('message');
+	let basicErrors = vscode.commands.registerCommand('haskell-debugging.showHighlight', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const document = editor.document;
-			const filename = document.fileName;
-			const uri = document.uri;
-
-			let diagnosticCollection = vscode.languages.createDiagnosticCollection("stuff");
-
-			const errors = testJson.errors;
-			const diagnostics = errors.map(error => error.locations.map(
-				l => new vscode.Diagnostic(new vscode.Range(l.fromLine, l.fromColumn, l.toLine, l.toColumn), error.message))).flat();
-			diagnosticCollection.set(uri, diagnostics)
+			const filePath = document.fileName;
+			const fileName = filePath.split("\\").pop()?.split(".").shift();
+			switch(fileName){
+				case "No-Instance":
+					updateDiagnostics(document, diagnosticsSet, noInstanceJson);
+					break;
+				case "Incompatibility":
+					updateDiagnostics(document, diagnosticsSet, incompatiblilityJson);
+					break;
+				default:
+					vscode.window.showInformationMessage('This function can only be used with Incompatibility.hs or no-instance.hs');
+					return;
+			}
+			
 			
 		}
 
 	});
-	
 
-	context.subscriptions.push(disposable);
+	let selectType = vscode.commands.registerCommand('haskell-debugging.startTypeQuestion', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const filePath = document.fileName;
+			const fileName = filePath.split("\\").pop()?.split(".").shift();
+			const uri = document.uri;
+			if (fileName != "Incompatibility"){
+				vscode.window.showInformationMessage('This function can only be used with Incompatibility.hs');
+				return;
+			}
+			
+
+			const diagnostics = incompatiblilityJson.errors.map(error => error.locations.map(
+				l => new vscode.Diagnostic(new vscode.Range(l.fromLine, l.fromColumn, l.toLine, l.toColumn), error.message))).flat();
+			
+			for (let item of diagnostics) {
+				item.code = CHECKINGTYPE;
+			}      
+			diagnosticsSet.set(uri, diagnostics); 
+		}
+
+		
+
+
+	});
+	context.subscriptions.push(basicErrors);
+	context.subscriptions.push(selectType);
+	context.subscriptions.push(
+		vscode.languages.registerCodeActionsProvider({ scheme: 'file', language: 'haskell' }, new TypeChooser(), {
+			providedCodeActionKinds: TypeChooser.providedCodeActionKinds
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('haskell-debugging.updateDiognostics', (json) => {
+			DOCUMENT ?
+			updateDiagnostics(DOCUMENT, diagnosticsSet, json) :
+			vscode.window.showInformationMessage('This function can only be used with Incompatibility.hs');
+		})
+	);
+
 }
 
 
 
 const jsonToDiagnostics = function(string: string) : vscode.Diagnostic[] {
 
-	interface error {
-		message: string
-		locations: location[]
-	}
-	interface location{
-		fromLine: number
-		toLine: number
-		fromColumn: number
-		toColumn: number
-	}
-
-	interface parsedJson {
-		fileName: string
-		moduleName: string
-		errors: error[] 
-	}
-
 	
 	const json : parsedJson= JSON.parse(string);
 
 	const diagnostics = json.errors.map((e : error) => e.locations.map(
 		l => new vscode.Diagnostic(new vscode.Range(l.fromLine, l.fromColumn, l.toLine, l.toColumn), e.message))).flat();
-
-		return diagnostics
-	} 
+	
+	return diagnostics;
+	} ;
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+const updateDiagnostics = function(document: vscode.TextDocument, diagnosticCollection: vscode.DiagnosticCollection, json: parsedJson){
+	const uri = document.uri;
+
+	const diagnostics = json.errors.map(error => error.locations.map(
+		l => new vscode.Diagnostic(new vscode.Range(l.fromLine, l.fromColumn, l.toLine, l.toColumn), error.message))).flat();
+
+	diagnosticCollection.set(uri, diagnostics);
+};
+
+
+export class TypeChooser implements vscode.CodeActionProvider {
+
+	public static readonly providedCodeActionKinds = [
+		vscode.CodeActionKind.QuickFix
+	];
+
+	provideCodeActions(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, context: vscode.CodeActionContext, token: vscode.CancellationToken): vscode.CodeAction[] {
+		console.log(context.diagnostics);
+		return context.diagnostics
+			.filter(diagnostic => diagnostic.code === CHECKINGTYPE)
+			.map(diagnostic => this.createCommandCodeAction(diagnostic)).flat();
+	}
+
+	private createCommandCodeAction(diagnostic: vscode.Diagnostic): vscode.CodeAction[] {
+		const action = new vscode.CodeAction('type is \'whoIsRight: Bool -> String\'', vscode.CodeActionKind.QuickFix);
+		action.command = { command: 'haskell-debugging.updateDiognostics', title: 'Set to string', arguments: [IncompatibilityStringJson]};
+		action.diagnostics = [diagnostic];
+		action.isPreferred = false;
+		const action2 = new vscode.CodeAction('type is \'whoIsRight: Num a => Bool -> a\'', vscode.CodeActionKind.QuickFix);
+		action2.command = { command: 'haskell-debugging.updateDiognostics', title: 'Set to number', arguments: [IncompatibilityNumberJson]};
+		action2.diagnostics = [diagnostic];
+		action2.isPreferred = false;
+		return [action, action2];
+	}
+}
